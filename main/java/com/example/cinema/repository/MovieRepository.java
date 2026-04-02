9ed3bc0e526c20e35b03cef32c126e31a924da04
firefly123-hsl <3101651940@qq.com>
2026-04-02 17:30:27 +0800

    Delete main directory

--- main/java/com/example/cinema/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-﻿package com.example.cinema.repository;
-
-import com.example.cinema.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-    List<Movie> findByTitleContainingIgnoreCaseOrActorsContainingIgnoreCase(String title, String actors);
-}
